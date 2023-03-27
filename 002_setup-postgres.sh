7281919631bf18409c0a6e4d329ef7544fa5a118
Timothy Wu <g39328006@ym.edu.tw>
2023-03-27 11:01:26 +0800

    <feat> Add pg_stat_statements

--- 002_setup-postgres.sh
+++ 002_setup-postgres.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 在 postgres.conf 中寫入您需要的變數
-echo "shared_preload_libraries = 'timescaledb,pg_cron'" >> ${PGDATA}/postgresql.conf
+echo "shared_preload_libraries = 'timescaledb,pg_cron,pg_stat_statements'" >> ${PGDATA}/postgresql.conf
 echo "cron.database_name = '${POSTGRES_DB:-postgres}'" >> ${PGDATA}/postgresql.conf
 
 pg_ctl restart 
--- install_pg_cron.sql
+++ install_pg_cron.sql
@@ -3,3 +3,4 @@
 
 -- 安裝 pg_cron 擴展
 CREATE EXTENSION IF NOT EXISTS pg_cron;
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
